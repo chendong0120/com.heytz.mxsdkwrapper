e1aef5b36559db81b042e1a53b76b5267da1c1a4
陈东东 <kevinchen@heytz.com>
2017-07-28 18:42:44 +0800

    优化Android配网 修改ios配网包

--- src/android/mxsdkwrapper.java
+++ src/android/mxsdkwrapper.java
@@ -2,8 +2,13 @@ package com.heytz.mxsdkwrapper;
 
 import android.content.Context;
 import android.util.Log;
-import com.mxchip.easylink.EasyLinkAPI;
-import com.mxchip.easylink.FTCListener;
+
+import io.fogcloud.sdk.easylink.api.EasyLink;
+import io.fogcloud.sdk.easylink.helper.EasyLinkCallBack;
+import io.fogcloud.sdk.easylink.helper.EasyLinkParams;
+import io.fogcloud.sdk.mdns.api.MDNS;
+import io.fogcloud.sdk.mdns.helper.SearchDeviceCallBack;
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
@@ -11,24 +16,17 @@ import org.apache.cordova.CordovaWebView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 /**
  * This class starts transmit to activation
  */
 public class mxsdkwrapper extends CordovaPlugin {
 
-    private static String TAG = "=====mxsdkwrapper.class====";
+    private static String TAG = "=====mxsdkwrapper====";
     private CallbackContext easyLinkCallbackContext;
-    private Context context;
-    //    private FTC_Service ftcService;
-    private String userName;
-    private String deviceLoginID;
-    private String devicePassword;
-    //  private String appSecretKey;
-    //  private int easylinkVersion;
-    private int activateTimeout;
-    private String activatePort;
-    private EasyLinkAPI elapi;
-
+    private EasyLink elink;
+    private String _SERV_NAME = "_easylink._tcp.local.";
+    private MDNS mdns;
 
     /**
      * Step 2.1 FTC Call back, process the response from MXChip Model.
@@ -37,7 +35,8 @@ public class mxsdkwrapper extends CordovaPlugin {
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
-        context = cordova.getActivity().getApplicationContext();
+        elink = new EasyLink(cordova.getActivity().getApplicationContext());
+        mdns = new MDNS(cordova.getActivity().getApplicationContext());
     }
 
     @Override
@@ -45,20 +44,19 @@ public class mxsdkwrapper extends CordovaPlugin {
         if (action.equals("setDeviceWifi")) {
             String wifiSSID = args.getString(0);
             String wifiKey = args.getString(1);
-
             if (wifiSSID == null || wifiSSID.length() == 0 || wifiKey == null || wifiKey.length() == 0) {
                 Log.e(TAG, "arguments error ===== empty");
                 return false;
             }
-
             easyLinkCallbackContext = callbackContext;
-            this.transmitSettings(wifiSSID, wifiKey);
+            this.startEl(wifiSSID, wifiKey);
+            startMDNS();
             return true;
         }
         if (action.equals("dealloc")) {
-//            final EasyLinkAPI elapi = new EasyLinkAPI(context);
             try {
                 stop();
+                callbackContext.success();
             } catch (Exception e) {
                 Log.e(TAG, e.getMessage());
             }
@@ -68,77 +66,109 @@ public class mxsdkwrapper extends CordovaPlugin {
     }
 
     /**
-     * Step1. Call FTC Service to start transmit settings.
-     *
-     * @param wifiSSID @desc wifi ssid
-     * @param wifiKey  @desc corresponding wifi key
+     * ????????
      */
-    private void transmitSettings(String wifiSSID, String wifiKey) {
-        Log.i(TAG, " Step1. Call FTC Service to transmit settings. SSID = " + wifiSSID + ", Password = " + wifiKey);
-        if (wifiSSID != null && wifiSSID.length() > 0 && wifiKey != null && wifiKey.length() > 0) {
-//            final EasyLinkAPI elapi = new EasyLinkAPI(context);
-            resetEasyLink();
-            elapi.startFTC(wifiSSID, wifiKey, new FTCListener() {
-                @Override
-                public void onFTCfinished(String ip,
-                                          String data) {
-                    elapi.stopEasyLink();
+    private void startEl(String wifiSSID, String wifiKey) {
+        EasyLinkParams easylinkPara = new EasyLinkParams();
+        easylinkPara.ssid = wifiSSID.trim();
+        easylinkPara.password = wifiKey.toString().trim();
+        easylinkPara.runSecond = 60000;
+        easylinkPara.sleeptime = 10;
 
-                    if (!"".equals(data)) {
-                        JSONObject jsonObj;
-                        try {
-                            jsonObj = new JSONObject(data);
+        elink.startEasyLink(easylinkPara, new EasyLinkCallBack() {
+            @Override
+            public void onSuccess(int code, String message) {
+                Log.d(TAG + "startEasyLink", code + message.toString());
+            }
 
-                            String deviceName = jsonObj.getString("N");
-                            final String deviceIP = jsonObj.getJSONArray("C")
-                                    .getJSONObject(1).getJSONArray("C")
-                                    .getJSONObject(3).getString("C");
-                            Log.i(TAG, "findedDeviceIP:" + deviceIP);
-                            final String deviceMac = "C89346"
-                                    + deviceName.substring(
-                                    deviceName.indexOf("(") + 1,
-                                    deviceName.length() - 1);
-                            JSONObject result = new JSONObject();
-                            result.put("name",deviceName);
-                            result.put("ip",deviceIP);
-                            result.put("mac",deviceMac);
+            @Override
+            public void onFailure(int code, String message) {
+                Log.d(TAG + "startEasyLink", code + message.toString());
+                if (easyLinkCallbackContext != null) {
+                    easyLinkCallbackContext.error("startEasyLink error");
+                    stopEl();
+                }
+            }
+        });
+    }
 
-                            easyLinkCallbackContext.success(result);
-                            //easyLinkCallbackContext.success("{\"ip\":\"" + deviceIP + "\",\"mac\":\"" + deviceMac + "\"}");
+    /**
+     * ????????
+     */
+    private void stopEl() {
 
+        elink.stopEasyLink(new EasyLinkCallBack() {
+            @Override
+            public void onSuccess(int code, String message) {
+                Log.d(TAG + "stopEasyLink", code + message.toString());
 
-                        } catch (JSONException e) {
-                            Log.e(TAG, e.getMessage());
-                            easyLinkCallbackContext.error("parse JSON obj error");
-                        }
-                    } else {
-                        Log.e(TAG, "socket data is empty!");
-                        easyLinkCallbackContext.error("FTC socket data empty");
+            }
+
+            @Override
+            public void onFailure(int code, String message) {
+                Log.d(TAG + "stopEasyLink", code + message.toString());
+            }
+        });
+    }
+
+    private void startMDNS() {
+        mdns.startSearchDevices(_SERV_NAME, new SearchDeviceCallBack() {
+            @Override
+            public void onSuccess(int code, String message) {
+                super.onSuccess(code, message);
+                Log.d(TAG + "startMDNS", code + message.toString());
+
+            }
+
+            @Override
+            public void onFailure(int code, String message) {
+                super.onFailure(code, message);
+                Log.d(TAG + "startMDNS", code + message.toString());
+                stopEl();
+                if (easyLinkCallbackContext != null) {
+                    easyLinkCallbackContext.error("mdns failure" + message);
+                }
+            }
+
+            @Override
+            public void onDevicesFind(int code, JSONArray deviceStatus) {
+                super.onDevicesFind(code, deviceStatus);
+                if (deviceStatus.length() > 0 && easyLinkCallbackContext != null) {
+                    try {
+                        JSONObject device = deviceStatus.getJSONObject(0);
+                        Log.d(TAG + "startMDNS", code + deviceStatus.toString());
+                        JSONObject result = new JSONObject();
+                        result.put("name", device.getString("Name"));
+                        result.put("ip", device.getString("IP"));
+                        result.put("mac", device.getString("MAC").replaceAll(":", ""));
+                        easyLinkCallbackContext.success(result);
+                        stopEl();
+                        stopMDNS();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
                     }
-                    //elapi.stopFTC();
-
                 }
+            }
+        });
+    }
 
-                @Override
-                public void isSmallMTU(int MTU) {
-                }
-            });
-        } else {
-            easyLinkCallbackContext.error("args error");
-        }
+    private void stopMDNS() {
+        mdns.stopSearchDevices(new SearchDeviceCallBack() {
+            @Override
+            public void onSuccess(int code, String message) {
+                super.onSuccess(code, message);
+            }
+        });
     }
 
     private void stop() {
-        if (elapi != null) {
-            elapi.stopEasyLink();
-            elapi.stopFTC();
-            elapi = null;
-        }
+        stopEl();
+        stopMDNS();
+        easyLinkCallbackContext = null;
     }
 
     private void resetEasyLink() {
         stop();
-        elapi = new EasyLinkAPI(context);
     }
 
 }
